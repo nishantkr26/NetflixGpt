1005d1d271d61bacaf53ab04a2b1d0ab9721d03b
Nishant Kumar <nkashyap514@gmail.com>
2025-08-19 22:50:26 +0530

    Setup the project && essential libraries

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='bg-amber-800 text-3xl'>
+        Hello World
+        This is India
+      </div>
+
+    </>
+  )
+}
+
+export default App
